5bbf5c53fbf233d21aac98e51df1bc7359073828
unknown <vitagrebennik@gmail.com>
2022-08-26 19:07:45 +0300

    add repo_name to BrouserRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/team-project-kapusta/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
