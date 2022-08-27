1b8fdafe67e9706db488e69fcd888732a8c83e4d
unknown <vitagrebennik@gmail.com>
2022-08-27 21:18:21 +0300

    add notification with zero balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,23 +1,47 @@
+import { useState } from 'react';
 import Section from '../common/Section';
+import s from './Balance.module.css';
 
 const Balance = () => {
+  const [balance, setBalance] = useState('');
+
+  const handleChange = e => {
+    setBalance(e.target.value);
+  };
+
   return (
     <>
       <Section>
-        <form>
-          <label htmlFor="balance">Balance:</label>
-          <input
-            id="#balance"
-            // className={s.input}
-            type="text"
-            name="name"
-            // value={balance}
-            // onChange={handleChange}
-            placeholder="00.00 UAH"
-            required
-          />
-          <button type="submit">Confirm</button>
-        </form>
+        <div className={s.container}>
+          <form className={s.form}>
+            <label htmlFor="balance" className={s.label}>
+              Balance:
+            </label>
+            <input
+              id="#balance"
+              className={s.input}
+              type="text"
+              name="balance"
+              value={balance}
+              onChange={handleChange}
+              required
+            />
+            <button type="submit" className={s.button}>
+              Confirm
+            </button>
+          </form>
+          {balance === '0' && (
+            <div className={s.popUpContainer}>
+              <p className={s.popUpText}>
+                Hello! To get started, enter the current balance of your
+                account!
+              </p>
+              <p className={s.popUpTextBottom}>
+                You can't spend money until you have it
+              </p>
+            </div>
+          )}
+        </div>
       </Section>
     </>
   );
