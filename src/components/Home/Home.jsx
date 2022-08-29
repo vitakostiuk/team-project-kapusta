92e60270cb002008cd86b8871cfdade3929b8a48
Anna Kysilchuk <anya.kislchuk@gmail.com>
2022-08-30 01:19:54 +0300

    react-datepicker installing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Balance from 'components/Balance';
+import Tabs from 'components/Table/Tabs/Tabs';
+import InputForm from 'components/Table/InputForm/InputForm';
 
 const Home = () => {
   return (
     <div>
       <Balance />
+      <Tabs />
+      <InputForm />
     </div>
   );
 };
