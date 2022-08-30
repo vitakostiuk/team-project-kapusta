d135fe8d9c49ece5981167796a71c7d448cb23a5
unknown <vitagrebennik@gmail.com>
2022-08-30 12:09:36 +0300

    add table summary

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Balance from 'components/Balance';
+import Summary from './Summary';
 
 const Home = () => {
   return (
     <div>
       <Balance />
+      <Summary />
     </div>
   );
 };
