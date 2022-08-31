c79f50f816f0c833d27153deab07f9c002996615
Drummberg <drummberg@gmail.com>
2022-09-01 01:26:42 +0300

    fix MainPage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,25 +1,26 @@
-// import { useSelector } from 'react-redux';
-// import authSelectors from '../../redux/feature/auth-selectors';
+import { useSelector } from 'react-redux';
+import authSelectors from '../../redux/feature/auth-selectors';
 import PropTypes from 'prop-types';
 import * as Styled from './MainPage.styled';
 import Container from '../Container';
 
 const MainPage = ({ children }) => {
-  // const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   return (
     <>
-      {/* з цим кодом дублюються сторінки */}
-      {/* {isLoggedIn && (
+      {isLoggedIn && (
         <Styled.MainAuth>
           <Styled.TopAuth></Styled.TopAuth>
           <Container>{children}</Container>
         </Styled.MainAuth>
-      )} */}
-      <Styled.MainSt>
-        <Styled.Top></Styled.Top>
-        <Container>{children}</Container>
-      </Styled.MainSt>
+      )}
+      {!isLoggedIn && (
+        <Styled.MainSt>
+          <Styled.Top></Styled.Top>
+          <Container>{children}</Container>
+        </Styled.MainSt>
+      )}
     </>
   );
 };
