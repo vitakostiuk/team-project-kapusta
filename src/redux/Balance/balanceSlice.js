da91e2949b1bb5d475669ce3ab1b06da59db0f82
unknown <vitagrebennik@gmail.com>
2022-08-31 15:41:09 +0300

    add redirect authorization, add balance state

---
+++ src/redux/Balance/balanceSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const balanceSlice = createSlice({
+  name: 'balance',
+  initialState: '00.00 UAH',
+  reducers: {
+    setBalance: (_, action) => action.payload,
+  },
+});
+
+export const { setBalance } = balanceSlice.actions;
+export default balanceSlice.reducer;
