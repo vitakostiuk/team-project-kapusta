d035fc65019aa7cc99818c6ca0123de074b4fedb
Denis-us <75166604+Denis-us@users.noreply.github.com>
2022-09-01 09:18:15 +0300

    потроху додаю функціонал

--- src/components/Report/Period/Period.jsx
+++ src/components/Report/Period/Period.jsx
@@ -1,20 +1,70 @@
-import React from 'react';
-import * as Styled from './Period.styled';
+import React, { useState, useEffect } from 'react';
+import s from './Period.module.css';
 import Arrow from '../Arrow';
 
 const Period = () => {
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
+
+  const months = [
+    'JANUARY',
+    'FEBRUARY',
+    'MARCH',
+    'APRIL',
+    'MAY',
+    'JUNE',
+    'JULY',
+    'AUGUST',
+    'SEPTEMBER',
+    'OCTOBER',
+    'NOVEMBER',
+    'DECEMBER',
+  ];
+
+  useEffect(() => {
+    const currentMonth = months[new Date().getMonth()];
+    setMonth(currentMonth);
+
+    const currentYear = new Date().getFullYear();
+    setYear(currentYear);
+  }, []);
+
+  const prevMonth = () => {
+    if (month === 'JANUARY') {
+      setYear(year - 1);
+      setMonth('DECEMBER');
+      return;
+    }
+    setMonth(months[months.indexOf(month) - 1]);
+  };
+
+  const nextMonth = () => {
+    if (month === 'DECEMBER') {
+      setYear(year + 1);
+      setMonth('JANUARY');
+      return;
+    }
+    setMonth(months[months.indexOf(month) + 1]);
+  };
+
   return (
-    <Styled.Container>
-      <Styled.Period>Current period:</Styled.Period>
-      <Styled.PeriodWrapper>
-        <Arrow />
-        <Styled.Date>
-          <Styled.Month>NOVEMBER</Styled.Month>
-          <Styled.Year>2020</Styled.Year>
-        </Styled.Date>
-        <Arrow rotate="true" />
-      </Styled.PeriodWrapper>
-    </Styled.Container>
+    <div className={s.container}>
+      <p className={s.period}>Current period:</p>
+      <div className={s.periodWrapper}>
+        <button className={s.btn} onClick={prevMonth}>
+          <Arrow />
+        </button>
+
+        <div className={s.date}>
+          <p className={s.month}>{month}</p>
+          <p className={s.year}>{year}</p>
+        </div>
+
+        <button className={s.btn} onClick={nextMonth}>
+          <Arrow rotate="true" />
+        </button>
+      </div>
+    </div>
   );
 };
 
