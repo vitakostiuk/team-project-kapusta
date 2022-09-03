63148015930efaeb2fe1437920ce557742b87128
Denis-us <75166604+Denis-us@users.noreply.github.com>
2022-09-03 17:34:37 +0300

    ssaf

--- src/components/Report/Expenses/Category/Category.jsx
+++ src/components/Report/Expenses/Category/Category.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import s from './Category.module.css';
 import Icons from '../../../../images/report/sprite-icons.svg';
 
-const Category = ({ name }) => {
+const Category = ({ price, categories }) => {
   const handleBtnClick = () => {};
 
   return (
     <li className={s.category}>
-      <p className={s.sum}>3 000.00</p>
+      <p className={s.sum}>{price}</p>
       <button className={s.btn} onClick={handleBtnClick}>
         <svg className={s.picture}>
-          <use xlinkHref={`${Icons}#icon-${name}`} />
+          <use xlinkHref={`${Icons}#icon-${categories}`} />
         </svg>
       </button>
-      <p className={s.nameOfCategory}>{name}</p>
+      <p className={s.nameOfCategory}>{categories}</p>
     </li>
   );
 };
 
-Category.propTypes = {
-  name: PropTypes.string.isRequired,
-};
+// Category.propTypes = {
+//   name: PropTypes.string.isRequired,
+// };
 
 export default Category;
