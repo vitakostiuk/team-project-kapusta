213d85a3aba66536a424b53d3cf0f1158286441f
unknown <vitagrebennik@gmail.com>
2022-09-05 21:27:03 +0300

    add url params

--- src/components/Table/InputForm/Calendar/Calendar.jsx
+++ src/components/Table/InputForm/Calendar/Calendar.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import style from './Calendar.module.css';
 
 const Calendar = ({ onChangeDate, startDate }) => {
+  const handleDateSelect = e => {};
+
   return (
     <DatePicker
       closeOnScroll={true}
       selected={startDate}
+      onSelect={handleDateSelect}
       onChange={date => onChangeDate(date)}
       className={style.calendar}
       dateFormat="dd.MM.yyyy"
