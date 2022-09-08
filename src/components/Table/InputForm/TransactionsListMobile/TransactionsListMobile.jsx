fb05d3e2a019999fc104f10c6050813a58fa45ed
Anna Kysilchuk <anya.kislchuk@gmail.com>
2022-09-08 20:44:55 +0300

    Fixed mobile Transactions List

--- src/components/Table/InputForm/TransactionsListMobile/TransactionsListMobile.jsx
+++ src/components/Table/InputForm/TransactionsListMobile/TransactionsListMobile.jsx
@@ -1,7 +1,6 @@
 import style from './TransactionsListMobile.module.css';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
 import { ReactComponent as DeletePic } from '../../../../images/delete.svg';
 import EllipsisText from 'react-ellipsis-text';
 import {
@@ -17,8 +16,6 @@ import ModalDelete from 'components/common/ModalDelete';
 
 const TransactionsListMobile = () => {
   const [isShowModal, setIsShowModal] = useState(false);
-  const type = useLocation().pathname;
-  // const screenWidth = useSelector(state => state.screenWidth);
 
   const date = useSelector(state => state.date);
   const expense = useGetTransactionsByExpenseQuery(date, {
@@ -32,31 +29,77 @@ const TransactionsListMobile = () => {
   const transExspenses = useSelector(state => state.transactions.expense);
   const transIncome = useSelector(state => state.transactions.income);
 
+  const transactions = () => {
+    let transArr = [];
+
+    transIncome?.map(
+      ({
+        date: { day, month, year },
+        description,
+        categories,
+        _id,
+        value,
+        income,
+        updatedAt,
+      }) =>
+        transArr.push({
+          date: { day, month, year },
+          description,
+          categories,
+          _id,
+          value,
+          income,
+          updatedAt: Date.parse(updatedAt),
+        }),
+    );
+    transExspenses?.map(
+      ({
+        date: { day, month, year },
+        description,
+        categories,
+        _id,
+        value,
+        income,
+        updatedAt,
+      }) =>
+        transArr.push({
+          date: { day, month, year },
+          description,
+          categories,
+          _id,
+          value,
+          income,
+          updatedAt: Date.parse(updatedAt),
+        }),
+    );
+
+    transArr.sort(
+      (firstItem, secondItem) => secondItem.updatedAt - firstItem.updatedAt,
+    );
+
+    return transArr;
+  };
+
+  const transactionsArr = transactions();
+
   useEffect(() => {
-    if (!expense?.isSuccess && type === '/expenses') {
+    if (!expense?.isSuccess) {
       dispatch(getTransExpenses(null));
       return;
     }
-    if (!income?.isSuccess && type === '/income') {
+    if (!income?.isSuccess) {
       dispatch(getTransIncome(null));
       return;
     }
 
-    if (type === '/expenses') {
+    if (expense) {
       dispatch(getTransExpenses(expense?.data?.transactions));
     }
 
-    if (type === '/income') {
+    if (income) {
       dispatch(getTransIncome(income?.data?.transactions));
     }
-  }, [
-    dispatch,
-    expense?.data?.transactions,
-    expense?.isSuccess,
-    income?.data?.transactions,
-    income?.isSuccess,
-    type,
-  ]);
+  }, [dispatch, expense, income]);
 
   const handleClick = () => {
     setIsShowModal(prevIsShowModal => !prevIsShowModal);
@@ -65,26 +108,9 @@ const TransactionsListMobile = () => {
   return (
     <div className={style.thamb}>
       <ul className={style.expensesIncomeList}>
-        {transExspenses &&
-          !expense.isFetching &&
-          transExspenses?.map(
-            ({ date: { day, month, year }, description, categories, _id }) => (
-              <li className={style.item} key={_id}>
-                <span className={style.itemName}>
-                  <EllipsisText text={`${description}`} length={Number(15)} />
-                </span>
-                <br />
-                <span
-                  className={style.itemDate}
-                >{`${day}.${month}.${year}`}</span>
-                <span className={style.itemCategory}>{categories}</span>
-                <hr className={style.line} />
-              </li>
-            ),
-          )}
-        {transIncome &&
-          !income.isFetching &&
-          transIncome?.map(
+        {transactionsArr &&
+          !transactionsArr.isFetching &&
+          transactionsArr?.map(
             ({ date: { day, month, year }, description, categories, _id }) => (
               <li className={style.item} key={_id}>
                 <span className={style.itemName}>
@@ -102,47 +128,29 @@ const TransactionsListMobile = () => {
       </ul>
 
       <ul className={style.expensesIncomeSum}>
-        {transExspenses &&
-          !expense.isFetching &&
-          transExspenses?.map(({ value, _id }) => (
-            <li className={style.itemSum} key={_id}>
-              <span className={style.sumExpense}>{`-${getNormalizedSum(
-                value,
-              )}`}</span>
-            </li>
-          ))}
-        {transIncome &&
-          !income.isFetching &&
-          transIncome?.map(({ value, _id }) => (
-            <li className={style.itemSum} key={_id}>
-              <span className={style.sumIncome}>{getNormalizedSum(value)}</span>
-            </li>
-          ))}
+        {transactionsArr &&
+          !transactionsArr.isFetching &&
+          transactionsArr?.map(({ value, _id, income }) =>
+            !income ? (
+              <li className={style.itemSum} key={_id}>
+                <span className={style.sumExpense}>{`-${getNormalizedSum(
+                  value,
+                )}`}</span>
+              </li>
+            ) : (
+              <li className={style.itemSum} key={_id}>
+                <span className={style.sumIncome}>
+                  {getNormalizedSum(value)}
+                </span>
+              </li>
+            ),
+          )}
       </ul>
+
       <ul className={style.expensesIncomeDel}>
-        {transExspenses &&
-          !expense.isFetching &&
-          transExspenses?.map(({ _id }) => (
-            <li className={style.itemDel} key={_id}>
-              <button
-                type="button"
-                className={style.deleteBtn}
-                onClick={handleClick}
-              >
-                <DeletePic />
-              </button>
-              {isShowModal && (
-                <ModalDelete
-                  onClick={handleClick}
-                  text="Are you sure?"
-                  id={_id}
-                />
-              )}
-            </li>
-          ))}
-        {transIncome &&
-          !income.isFetching &&
-          transIncome?.map(({ _id }) => (
+        {transactionsArr &&
+          !transactionsArr.isFetching &&
+          transactionsArr?.map(({ _id }) => (
             <li className={style.itemDel} key={_id}>
               <button
                 type="button"
@@ -166,3 +174,158 @@ const TransactionsListMobile = () => {
 };
 
 export default TransactionsListMobile;
+
+// const TransactionsListMobile = () => {
+//   const [isShowModal, setIsShowModal] = useState(false);
+//   // const screenWidth = useSelector(state => state.screenWidth);
+
+//   const date = useSelector(state => state.date);
+//   const expense = useGetTransactionsByExpenseQuery(date, {
+//     refetchOnMountOrArgChange: true,
+//   });
+//   const income = useGetTransactionsByIncomeQuery(date, {
+//     refetchOnMountOrArgChange: true,
+//   });
+//   // const trans = useGetTransactionsAllQuery(date, {
+//   //   refetchOnMountOrArgChange: true,
+//   // });
+
+//   const dispatch = useDispatch();
+//   const transExspenses = useSelector(state => state.transactions.expense);
+//   const transIncome = useSelector(state => state.transactions.income);
+//   // const transAll = useSelector(state => state.transactions);
+//   // console.log(transAll);
+
+//   useEffect(() => {
+//     if (!expense?.isSuccess) {
+//       dispatch(getTransExpenses(null));
+//       return;
+//     }
+//     if (!income?.isSuccess) {
+//       dispatch(getTransIncome(null));
+//       return;
+//     }
+
+//     if (expense) {
+//       dispatch(getTransExpenses(expense?.data?.transactions));
+//     }
+
+//     if (income) {
+//       dispatch(getTransIncome(income?.data?.transactions));
+//     }
+//   }, [dispatch,
+//     expense,
+//     expense?.data?.transactions,
+//     expense?.isSuccess,
+//     income,
+//     income?.data?.transactions,
+//     income?.isSuccess]);
+
+//   const handleClick = () => {
+//     setIsShowModal(prevIsShowModal => !prevIsShowModal);
+//   };
+
+//   return (
+//     <div className={style.thamb}>
+//       <ul className={style.expensesIncomeList}>
+//         {transExspenses &&
+//           !expense.isFetching &&
+//           transExspenses?.map(
+//             ({ date: { day, month, year }, description, categories, _id }) => (
+//               <li className={style.item} key={_id}>
+//                 <span className={style.itemName}>
+//                   <EllipsisText text={`${description}`} length={Number(15)} />
+//                 </span>
+//                 <br />
+//                 <span
+//                   className={style.itemDate}
+//                 >{`${day}.${month}.${year}`}</span>
+//                 <span className={style.itemCategory}>{categories}</span>
+//                 <hr className={style.line} />
+//               </li>
+//             ),
+//           )}
+//         {transIncome &&
+//           !income.isFetching &&
+//           transIncome?.map(
+//             ({ date: { day, month, year }, description, categories, _id }) => (
+//               <li className={style.item} key={_id}>
+//                 <span className={style.itemName}>
+//                   <EllipsisText text={`${description}`} length={Number(15)} />
+//                 </span>
+//                 <br />
+//                 <span
+//                   className={style.itemDate}
+//                 >{`${day}.${month}.${year}`}</span>
+//                 <span className={style.itemCategory}>{categories}</span>
+//                 <hr className={style.line} />
+//               </li>
+//             ),
+//           )}
+//       </ul>
+
+//       <ul className={style.expensesIncomeSum}>
+//         {transExspenses &&
+//           !expense.isFetching &&
+//           transExspenses?.map(({ value, _id }) => (
+//             <li className={style.itemSum} key={_id}>
+//               <span className={style.sumExpense}>{`-${getNormalizedSum(
+//                 value,
+//               )}`}</span>
+//             </li>
+//           ))}
+//         {transIncome &&
+//           !income.isFetching &&
+//           transIncome?.map(({ value, _id }) => (
+//             <li className={style.itemSum} key={_id}>
+//               <span className={style.sumIncome}>{getNormalizedSum(value)}</span>
+//             </li>
+//           ))}
+//       </ul>
+//       <ul className={style.expensesIncomeDel}>
+//         {transExspenses &&
+//           !expense.isFetching &&
+//           transExspenses?.map(({ _id }) => (
+//             <li className={style.itemDel} key={_id}>
+//               <button
+//                 type="button"
+//                 className={style.deleteBtn}
+//                 onClick={handleClick}
+//               >
+//                 <DeletePic />
+//               </button>
+//               {isShowModal && (
+//                 <ModalDelete
+//                   onClick={handleClick}
+//                   text="Are you sure?"
+//                   id={_id}
+//                 />
+//               )}
+//             </li>
+//           ))}
+//         {transIncome &&
+//           !income.isFetching &&
+//           transIncome?.map(({ _id }) => (
+//             <li className={style.itemDel} key={_id}>
+//               <button
+//                 type="button"
+//                 className={style.deleteBtn}
+//                 onClick={handleClick}
+//               >
+//                 <DeletePic />
+//               </button>
+//               {isShowModal && (
+//                 <ModalDelete
+//                   onClick={handleClick}
+//                   text="Are you sure?"
+//                   id={_id}
+//                 />
+//               )}
+//             </li>
+//           ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default TransactionsListMobile;
