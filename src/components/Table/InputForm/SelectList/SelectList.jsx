5e25c4fef054e9349742998ce0e9258a0c36228e
unknown <vitagrebennik@gmail.com>
2022-09-16 12:38:36 +0300

    finished styling select

--- src/components/Table/InputForm/SelectList/SelectList.jsx
+++ src/components/Table/InputForm/SelectList/SelectList.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Select from 'react-select';
-import style from './SelectList.module.css';
 import { useGetTransactionsQuery } from 'redux/report/transactionsApi';
-import DropDown from './DropDown';
-// import './SelectList.css';
 import './SelectList.scss';
 
 const SelectList = ({ onChangeCategory, onChangeId }) => {
@@ -39,13 +36,7 @@ const SelectList = ({ onChangeCategory, onChangeId }) => {
     }
   }, [data, onChangeId, value]);
 
-  // const handleChangeCategory = e => {
-  //   setValue(e.target.value);
-  //   onChangeCategory(e.target.value);
-  // };
-
   const handleChangeCategory = e => {
-    // console.log(e);
     setValue(e.value);
     onChangeCategory(e.value);
   };
@@ -85,135 +76,7 @@ const SelectList = ({ onChangeCategory, onChangeId }) => {
         />
       )}
     </>
-    // <select
-    //   className={style.selectList}
-    //   name="category"
-    //   title="Select an item from the list"
-    //   onChange={handleChangeCategory}
-    //   required
-    // >
-    //   <option className={style.selectItem} value="Product category">
-    //     Product category
-    //   </option>
-    //   {(type === '/expenses' || type === '/expenses/input') &&
-    //     categoriesExpense.map(({ _id, title }) => (
-    //       <option key={title} value={title} className={style.selectItem}>
-    //         {title}
-    //       </option>
-    //     ))}
-    //   {(type === '/income' || type === '/income/input') &&
-    //     categoriesIncome.map(({ _id, title }) => (
-    //       <option key={title} value={title} className={style.selectItem}>
-    //         {title}
-    //       </option>
-    //     ))}
-    // </select>
   );
 };
 
 export default SelectList;
-
-// // WITH MATERIAL UI // //
-// import { useState, useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
-// import style from './SelectList.module.css';
-// import { useGetTransactionsQuery } from 'redux/report/transactionsApi';
-// import Box from '@mui/material/Box';
-// import Select from '@mui/material/Select';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormControl from '@mui/material/FormControl';
-// import InputLabel from '@mui/material/InputLabel';
-// import TextField from '@mui/material/TextField';
-
-// const SelectList = ({ onChangeCategory, onChangeId }) => {
-//   const [categoriesExpense, setCategoriesExpense] = useState([]);
-//   const [categoriesIncome, setCategoriesIncome] = useState([]);
-//   const [value, setValue] = useState('');
-//   const [id, setId] = useState('');
-//   const { data } = useGetTransactionsQuery();
-
-//   const type = useLocation().pathname;
-
-//   useEffect(() => {
-//     if (data) {
-//       const categories = data.data?.map(({ _id, title, type }) => ({
-//         id: _id,
-//         title,
-//         type,
-//       }));
-
-//       setCategoriesExpense(
-//         categories.filter(category => category.type === 'expenses'),
-//       );
-//       setCategoriesIncome(
-//         categories.filter(category => category.type === 'income'),
-//       );
-//       const findId = categories.find(category => category.title === value);
-//       if (findId) {
-//         setId(findId.id);
-//         onChangeId(findId.id);
-//       }
-//     }
-//   }, [data, id, onChangeId, value]);
-
-//   const handleChangeCategory = e => {
-//     setValue(e.target.value);
-//     onChangeCategory(e.target.value);
-//   };
-
-//   return (
-//     <FormControl>
-//       {/* <InputLabel id="demo-simple-select-label" className={style.inputLabel}>
-//           Product category
-//         </InputLabel> */}
-//       <InputLabel
-//         variant="filled"
-//         htmlFor="uncontrolled-native"
-//         className={style.inputLabel}
-//       >
-//         Product category
-//       </InputLabel>
-//       <Select
-//         labelId="demo-simple-select-label"
-//         id="demo-simple-select"
-//         className={style.selectList}
-//         sx={{
-//           '& .MuiInputBase-root, .MuiOutlinedInput-root, MuiSelect-root': {
-//             outline: 'none',
-//             border: '2px solid #f6f7fc',
-//           },
-//         }}
-//         name="category"
-//         value={value}
-//         title="Select an item from the list"
-//         onChange={handleChangeCategory}
-//         displayEmpty={false}
-//         required
-//       >
-//         {(type === '/expenses' || type === '/expenses/input') &&
-//           categoriesExpense.map(({ _id, title }) => (
-//             <MenuItem key={title} value={title} className={style.selectItem}>
-//               {title}
-//             </MenuItem>
-//           ))}
-//         {(type === '/income' || type === '/income/input') &&
-//           categoriesIncome.map(({ _id, title }) => (
-//             <MenuItem key={title} value={title} className={style.selectItem}>
-//               {title}
-//             </MenuItem>
-//           ))}
-//       </Select>
-//     </FormControl>
-//   );
-// };
-
-// export default SelectList;
-// <DropDown
-// value={value}
-// handleChange={handleChangeCategory}
-// items={[
-//   { key: 'Last 7 Days', value: 7 },
-//   { key: 'Last 28 Days', value: 28 },
-//   { key: 'Last 90 Days', value: 90 },
-// ]}
-// />
